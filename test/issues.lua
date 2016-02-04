e17db1610250f8e86b5645d13b8d1e7e8c2fd891
Alban Linard <alban@linard.fr>
2016-02-04 17:12:03 +0100

    Use pairs instead of remove contents.

--- test/issues.lua
+++ test/issues.lua
@@ -437,7 +437,7 @@ describe ("issue #20", function ()
       [default] = { v = 0 },
     }
     local res = {}
-    for k, v in Layer.contents (layer.a) do
+    for k, v in Layer.pairs (layer.a) do
       res [k] = v
     end
     assert.are.equal (res.x, 1)
